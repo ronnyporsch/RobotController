93a4d618848e2360fa1c86e69d630ced6af69cb7
Ronny Porsch <ronny.porsch@b-tu.de>
2025-04-24 16:56:46 +0200

    increased default window size

--- composeApp/src/desktopMain/kotlin/de/ronnyporsch/robot_controller/main.kt
+++ composeApp/src/desktopMain/kotlin/de/ronnyporsch/robot_controller/main.kt
@@ -1,12 +1,16 @@
 package de.ronnyporsch.robot_controller
 
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "RobotController",
+        state = WindowState(size = DpSize(800.dp, 800.dp),)
     ) {
         App()
     }
